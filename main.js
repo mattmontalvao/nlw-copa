051adfbacf26ce2d74eb11130754daa9d18c7a8e
Montalvão <contatomattmontalvao@gmail.com>
2022-11-05 15:31:45 -0300

    add main.js

--- main.js
+++ main.js
@@ -25,15 +25,15 @@ document.querySelector("#cards").innerHTML =
   createCard(
     "24/11",
     "quinta",
-    createGame("switzerland", "07:00", "cameroon") /* +
+    createGame("switzerland", "07:00", "cameroon") +
       createGame("portugal", "13:00", "ghana") +
-      createGame("brazil", "16:00", "serbia") */
+      createGame("brazil", "16:00", "serbia")
   ) +
   createCard(
     "28/11",
     "segunda",
-    createGame("south-korea", "10:00", "ghana") /* +
+    createGame("south-korea", "10:00", "ghana") +
       createGame("brazil", "13:00", "switzerland") +
-      createGame("portugal", "16:00", "uruguai") */
+      createGame("portugal", "16:00", "uruguai")
   ) +
   createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
