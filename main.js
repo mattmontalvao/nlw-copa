901f2e66e0cc9c7663c00eceb3967010dba0924e
Montalvão <contatomattmontalvao@gmail.com>
2022-11-18 22:12:34 -0300

    Cardápio Aloha 1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function createGame(player1, hour, player2) {
+/* function createGame(player1, hour, player2) {
   return `
   <li>
     <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
@@ -6,34 +6,128 @@ function createGame(player1, hour, player2) {
     <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
   </li>
 `
-}
+} */
 
-let delay = 0
-function createCard(date, day, games) {
-  delay = delay + 0.1
+let tea = 0
+function sabores() {
+  tea = tea + 0.1
   return `
-  <div class="card" style="animation-delay: ${delay}s">
-    <h2>${date} <span>${day}</span></h2>
+  <div class="card">
+    <h2>Chá Mate</h2>
+    <ul id="epp">
+      <li><strong>
+      Limão 
+      </strong><br>
+      <span>Chá mate adoçado, com suco de limão.</span>
+      </li>
+
+      <li><strong>
+      Pêssego 
+      </strong><br>
+      <span>Chá mate adoçado, batido com polpa de pêssego.</span>
+      </li>
+
+      <li><strong>
+      Cajá
+      </strong><br>
+      <span>Chá mate adoçado, batido com polpa de cajá.</span>
+      </li>
+
+      <li><strong>
+      Tangerina
+      </strong><br>
+      <span>Chá mate adoçado, batido com polpa de tangerina.</span>
+      </li>
+
+      <li><strong>
+      Cupuaçu
+      </strong><br>
+      <span>Chá mate adoçado, batido com polpa de cupuaçu.</span>
+      </li>
+
+      <li><strong>
+      Tamarindo
+      </strong><br>
+      <span>Chá mate adoçado, batido com polpa de tamarindo.</span>
+      </li>
+
+      <li><strong>
+      Café
+      </strong><br>
+      <span>Chá mate adoçado, batido com café 100% arábica e com um toque de limão.</span>
+      </li>
+    </ul>
+
     <ul>
-      ${games}
+      <h2>Chá Preto</h2>
+      <li><strong>
+      Limão Siciliano
+      </strong><br>
+      <span>Chá preto adoçado, batido com limão siciliano.</span>
+      </li>
+    </ul>
+
+    <ul>
+      <h2>Chá de Hibisco</h2>
+      <li><strong>
+      Canela e Limão
+      </strong><br>
+      <span>Chá de hibisco com canela adoçado, batido com limão.</span>
+      </li>
+
+      <li><strong>
+      Limão
+      </strong><br>
+      <span>Chá de hibisco adoçado, batido com suco de limão.</span>
+      </li>
+    </ul>
+
+    <ul>
+      <h2>Chá Verde</h2>
+      <li><strong>
+      Hortelã e Limão
+      </strong><br>
+      <span>Chá verde com hortelã adoçado, batido com suco de limão.</span>
+      </li>
+
+      <li><strong>
+      Hibisco e Limão
+      </strong><br>
+      <span>Chá verde com hibisco adoçado, batido com suco de limão.</span>
+      </li>
+    </ul>
+
+    <ul>
+      <h2>Chá de Capim-Cidreira</h2>
+      <li><strong>
+      Manga
+      </strong><br>
+      <span>Chá de capim-cidreira adoçado, batido com polpa de manga e um toque de limão.</span>
+      </li>
+
+      <li><strong>
+      Tangerina
+      </strong><br>
+      <span>Chá de capim-cidreira adoçado, batido com polpa de tangerina e um toque de limão.</span>
+      </li>
+    </ul>
+
+    <ul>
+      <h2>Edições Limitadas</h2>
+      <li><strong class="halloween">
+      Especial Halloween
+      </strong><br>
+      <span>Chá mate com groselha adoçado, com um toque de limão.</span>
+      </li>
+
+      <li><strong class="natal">
+      Especial de Natal
+      </strong><br>
+      <span>Chá preto com frutas vermelhas adoçado, com um toque de limão.</span>
+      </li>
     </ul>
   </div>
   `
 }
 
-document.querySelector("#cards").innerHTML =
-  createCard(
-    "24/11",
-    "quinta",
-    createGame("switzerland", "07:00", "cameroon") +
-      createGame("portugal", "13:00", "ghana") +
-      createGame("brazil", "16:00", "serbia")
-  ) +
-  createCard(
-    "28/11",
-    "segunda",
-    createGame("south-korea", "10:00", "ghana") +
-      createGame("brazil", "13:00", "switzerland") +
-      createGame("portugal", "16:00", "uruguai")
-  ) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+document.querySelector("#cards").innerHTML = sabores()
